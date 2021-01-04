9edb82e44384543093ab8ffbbe5e48fb9809149f
Your Name <manishgupta_tcsdelhi@yahoo.com>
2021-01-04 09:29:03 +0530

    cradles

--- Rope.js
+++ Rope.js
@@ -1,19 +1,18 @@
 class Rope{
-    constructor(body1,body2,offsetX,offsetY){
-        this.offsetX=offsetX,
-        this.offsetY=offsetY
-      var options={
+    constructor(body1,pointB){
+       var options={
+          isStatic:false,
           bodyA:body1,
-          bodyB:body2,
           length: 400,
-          pointB:{x:this.offsetX,y:this.offsetY}
+          pointB:pointB
       }
+      this.pointB=pointB
       this.rope=Matter.Constraint.create(options)
       World.add(world,this.rope);
     }
     display(){
         strokeWeight(4);
         stroke("black");
-        line(this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.rope.bodyB.position.x,this.rope.bodyB.position.y);
+        line(this.rope.bodyA.position.x,this.rope.bodyA.position.y,this.pointB.x,this.pointB.y);
     }
 }
